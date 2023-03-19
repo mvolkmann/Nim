c5b7dd67415a6b660a9de2f7193207b19afb477a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-03-19 11:02:05 -0500

    Initial Commit

---
+++ Nim/NimApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimApp.swift
+//  Nim
+//
+//  Created by Mark Volkmann on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct NimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
