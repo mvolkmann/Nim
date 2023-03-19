d9acc84662a93307bf974205bf804c366d4367fa
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-03-19 12:46:37 -0500

    progress

--- Nim/ContentView.swift
+++ Nim/ContentView.swift
@@ -3,7 +3,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Nim").font(.largeTitle)
+            Text("Nim")
+                .font(.largeTitle)
+                .padding(.bottom)
+            Text("""
+            Take turns clicking squares to remove it
+            and all squares to its right in the same row.
+            The computer will do the same.
+            The player that removes the last square loses.
+            """)
             Nim()
         }
         .padding()
