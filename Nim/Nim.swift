f6e933961f3a7fb725fe516dc2b39e4a4f03d054
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-03-19 12:23:19 -0500

    progress

--- Nim/Nim.swift
+++ Nim/Nim.swift
@@ -3,34 +3,66 @@ import SwiftUI
 struct Nim: View {
     private let tabSize = 40.0
 
-    @State private var rows = [3, 5, 7]
+    @State private var computerWon = false
+    @State private var rows = [7, 5, 3]
 
     private var gameOver: Bool {
         rows.allSatisfy { count in count == 0 }
     }
 
-    private func removeTabs(row: Int, count: Int) {
-        rows[row] = count - 1
+    private func makeMove() {
+        guard !gameOver else { return }
+
+        var foundMove = false
+
+        // Find the first move that results in a score of zero.
+        for (index, count) in rows.enumerated() {
+            if count == 0 { continue }
+
+            let options = Array(1 ... count)
+            for count in options {
+                var board = rows
+                board[index] -= count
+                if score(board) == 0 {
+                    // print("Computer is removing \(count) from row \(index + 1)")
+                    removeTabs(row: index, count: count)
+                    foundMove = true
+                    break
+                }
+            }
+
+            if foundMove { break }
+        }
+
+        if !foundMove {
+            // Just remove one from any non-empty row.
+            for (index, count) in rows.enumerated() {
+                if count > 0 {
+                    // print("Computer is removing 1 from row \(index + 1)")
+                    removeTabs(row: index, count: 1)
+                    break
+                }
+            }
+        }
     }
 
-    private func score(rows: [Int]) -> Int {
-        let sortedRows = rows.sorted()
-        if sortedRows == [0, 1, 1] { return 1 } // not winning
-        if sortedRows == [1, 1, 1] { return 0 } // winning
-        if sortedRows == [0, 0, 1] { return 0 } // winning
+    private func removeTabs(row: Int, count: Int, byHuman: Bool = false) {
+        rows[row] -= count
+        if gameOver { computerWon = byHuman }
+    }
 
-        /*
-         var result = 0
-         for row in rows {
-             result ^= row
-         }
-         return result
-         */
+    private func score(_ rows: [Int]) -> Int {
+        let sortedRows = rows.sorted()
+        if sortedRows == [1, 1, 1] { return 0 } // winning
+        if sortedRows == [0, 1, 1] { return 1 } // not winning
+        if sortedRows == [0, 0, 1] { return 0 } // winning
         return rows.reduce(0) { acc, count in acc ^ count }
     }
 
     var body: some View {
         VStack {
+            Button("Computer Goes First") { makeMove() }
+
             ForEach(Array(rows.enumerated()), id: \.offset) { index, count in
                 if index > 0 {
                     Divider().overlay(.black)
@@ -42,7 +74,14 @@ struct Nim: View {
                     } else {
                         ForEach(1 ... count, id: \.self) { n in
                             Button(
-                                action: { removeTabs(row: index, count: n) },
+                                action: {
+                                    removeTabs(
+                                        row: index,
+                                        count: rows[index] - n + 1,
+                                        byHuman: true
+                                    )
+                                    makeMove()
+                                },
                                 label: {
                                     Color.pink
                                         .frame(width: tabSize, height: tabSize)
@@ -55,9 +94,11 @@ struct Nim: View {
             }
 
             if gameOver {
-                Text("Game Over!").font(.title)
+                Text("Game Over").font(.title)
+                Text((computerWon ? "The computer" : "You") + " won!")
+                    .font(.headline)
             } else {
-                Text("Score = \(score(rows: rows))")
+                Text("Score = \(score(rows))")
             }
 
             Spacer()
